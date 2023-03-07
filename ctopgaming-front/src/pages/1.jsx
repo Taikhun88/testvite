d7dcb2434554190b99b5d67295ecf35c2385f5d9
Tai KHUN <taiyang78190@gmail.com>
2023-03-07 16:06:38 +0100

    test components in app.jsx --DONE

---
+++ ctopgaming-front/src/pages/1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function test1(props) {
+  return (
+    <h1>test1</h1>
+  )
+}
+
+export default test1
---
+++ ctopgaming-front/src/pages/2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function test2(props) {
+  return (
+    <h1>test2</h1>
+  )
+}
+
+export default test2 
